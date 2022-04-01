21ab11e3d54b3918284dc0925539c3b76ae32b4f
Mudit Rana <87364334+muditr99@users.noreply.github.com>
2022-04-02 01:13:26 +0530

    changing the state from array to object

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import React from 'react';
 
+import { addMovies } from '../actions';
+
 class App extends React.Component {
   componentDidMount () {
     const { store } = this.props;
@@ -12,31 +14,35 @@ class App extends React.Component {
     })
     // make api call
     // dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data,
-    })
+    // store.dispatch({
+    //   type: 'ADD_MOVIES',
+    //   movies: data,
+    // })
+
+    store.dispatch(addMovies(data));
 
     console.log('STATE', this.props.store.getState());
   }
 
   render () {
-  const  movies = this.props.store.getState(); 
+  const { list } = this.props.store.getState(); // { list : [], favorites : []}
   console.log('RENDER');
   return (
     <div className="App">
       <Navbar />
       <div className="main">
+
         <div className="tabs">
-        <div className="tab">Movies</div>
-        <div className="tab">Favorites</div>
+          <div className="tab">Movies</div>
+          <div className="tab">Favorites</div>
         </div>
 
         <div className="list">
-          {movies.map((movie, index) => {
+          {list.map((movie, index) => {
             return <MovieCard movie = {movie} key={`movie-${index}`} />
           })}
         </div>
+
       </div>
     </div>
    );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,17 @@
-export default function movies (state = [], action) {// state is current state of store
-    if (action.type === 'ADD_MOVIES') {
-        return action.movies;
+import { ADD_MOVIES } from "../actions";
+
+const initialMoviesState = {
+    list : [],
+    favorites : []
+}
+
+export default function movies (state = initialMoviesState, action) {// state is current state of store
+    if (action.type === ADD_MOVIES) {
+        return {
+            ...state,
+            list : action.movies
+        };
     }
     return state;
 }
+
